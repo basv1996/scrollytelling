36bae140807b1080c7a8b562872f596aee918fe3
basv1996 <bas.vugts@gmail.com>
2022-01-14 21:03:20 +0100

    added scrollema

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,22 @@ const circles = d3
 .attr("cy", height / 2)
 .attr("r", 10)
 .style("fill", "steelblue")
+
+function moveLeft() {
+    circles
+    .transition()
+    .duration(750)
+    .attr("cx", 10)
+}
+function moveX() {
+    circles
+    .transition()
+    .duration(750)
+    .attr("cx", (d)=> d.x)
+}
+function moveY() {
+    circles
+    .transition()
+    .duration(750)
+    .attr("cx", (d)=> d.y)
+}
